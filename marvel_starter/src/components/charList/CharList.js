fb3522177b868a446b2b4ed8e31c123396530977
Albert <dony.nyc@gmail.com>
2023-12-25 19:59:28 +0100

    charList-component done

--- marvel_starter/src/components/charList/CharList.js
+++ marvel_starter/src/components/charList/CharList.js
@@ -6,39 +6,39 @@ const CharList = () => {
         <div className="char__list">
             <ul className="char__grid">
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
             </ul>
--- my-layout-try/src/components/app/App.js
+++ my-layout-try/src/components/app/App.js
@@ -1,5 +1,6 @@
 import AppHeader from "../appHeader/appHeader";
 import RandomChar from "../randomChar/randomChar";
+import CharList from "../charList/charList";
 
 import decoration from "../../resources/img/vision.png";
 
@@ -9,6 +10,10 @@ function App() {
       <AppHeader />
       <main>
         <RandomChar />
+        <div class="char__content">
+          <CharList />
+
+        </div>
       </main>
       <img className="bg-decoration" src={decoration} alt="vision" />
     </div>
